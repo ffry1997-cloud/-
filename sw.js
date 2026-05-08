153b18f09170c3b908bfc0e1ab34916fefddf87f
ffry1997-cloud <ffry1997@gmail.com>
2026-05-08 17:49:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-plans-v2';
+const CACHE_NAME = 'my-plans-v3';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(names => Promise.all(names.map(n => caches.delete(n))))));
 self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
